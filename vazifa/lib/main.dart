28720bae699e5bb5b1971ab350fabac5548546a8
Muhammad <149857879+muhammaddevuz@users.noreply.github.com>
2024-05-29 09:10:23 +0500

    aa

--- vazifa/lib/main.dart
+++ vazifa/lib/main.dart
@@ -16,8 +16,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-                debugShowCheckedModeBanner: false,
-                home: HomeScreen(),
-              );
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
   }
 }
